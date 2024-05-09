606cb5d13aebe8d9545aac03d15601f14b610718
Etienne Baumgartner <etienne.baumgartner@devoteamgcloud.com>
2024-05-09 15:21:10 +0200

    fixed admin tests

--- test/00-admin.ts
+++ test/00-admin.ts
@@ -7,8 +7,10 @@ describe("BC24-Admin", function () {
   let transporter: any;
   let slaughterer: any;
   let breeder: any;
-  let contract: any;
   let random: any;
+  let animalContract: any;
+  let roleAccessContract: any;
+  let ownerAndCategoryMapperContract: any;
 
   let setupService: any;
 
@@ -24,33 +26,39 @@ describe("BC24-Admin", function () {
     transporter = setupService.transporter;
     slaughterer = setupService.slaughterer;
     random = setupService.random;
-    contract = setupService.contract;
-  });
-
-  it("Test contract", async function () {
-    expect(await contract.uri(0)).to.equal("");
+    animalContract = setupService.animalContract;
+    roleAccessContract = setupService.roleAccessContract;
+    ownerAndCategoryMapperContract =
+      setupService.ownerAndCategoryMapperContract;
   });
 
   it("should only let default admin assign roles", async function () {
     await expect(
-      contract.connect(minter).grantRoleToAddress(minter.address, "MINTER_ROLE")
+      roleAccessContract
+        .connect(minter)
+        .grantRoleToAddress(minter.address, "MINTER_ROLE")
     ).to.be.revertedWithCustomError(
-      contract,
+      ownerAndCategoryMapperContract,
       "AccessControlUnauthorizedAccount"
     );
   });
 
   it("should assign MINTER_ROLE role to an address", async function () {
-    const tx = await contract
+    const tx = await roleAccessContract
       .connect(defaultAdmin)
       .grantRoleToAddress(random.address, "MINTER_ROLE");
     const receipt = await tx.wait();
-    const RoleGrantedEvent = contract.filters.RoleGranted(null, null, null);
-    const events = await contract.queryFilter(
+    const RoleGrantedEvent = roleAccessContract.filters.RoleGranted(
+      null,
+      null,
+      null
+    );
+    const events = await roleAccessContract.queryFilter(
       RoleGrantedEvent,
       receipt.blockNumber,
       receipt.blockNumber
     );
+
     assert.equal(events.length, 1, "Should have emitted one RoleGranted event");
     const eventArgs = events[0].args;
     assert.equal(
